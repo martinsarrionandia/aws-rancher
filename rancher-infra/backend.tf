79f3fffd07411ba36d56914fb5db4150403149e1
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-10-19 01:05:28 +0100

    No Vars Allowed FFS

--- rancher-infra/backend.tf
+++ rancher-infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = var.domain_name
+    bucket  = "sarrionandia.co.uk"
     key     = "terraform-state/aws-rancher/rancher-infra/terraform.tfstate"
     region  = "eu-west-1"
     encrypt = true
