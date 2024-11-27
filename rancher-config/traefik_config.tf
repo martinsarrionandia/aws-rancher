c6cff2177c183aea116735c6e1d06d2d4c0d80b3
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-11-27 22:34:55 +0000

    Move Traefik config to terraform

--- rancher-config/traefik_config.tf
+++ rancher-config/traefik_config.tf
@@ -1,8 +1,46 @@
-resource "kubernetes_manifest" "traefik-helm-config" {
-  manifest = yamldecode(templatefile("${path.module}/templates/traefik_config.yaml",
-    {
-      traefik-log-level              = var.traefik-log-level,
-      traefik-access-log             = var.traefik-access-log,
-      traefik-external-access-policy = var.traefik-external-access-policy
-  }))
+resource "helm_release" "traefik" {
+  namespace  = "traefik"
+  name       = "traefik"
+  repository = "https://traefik.github.io/charts"
+  chart      = "traefik"
+  version    = "33.1.0-rc1"
+  values     = [local.traefik-helm-manifest]
+  replace    = true
+  force_update = true
+  upgrade_install = true
+}
+
+locals {
+  traefik-helm-manifest = <<EOF
+deployment:
+  additionalVolumes:
+  - name: plugins
+    persistentVolumeClaim:
+      claimName: "${kubernetes_persistent_volume_claim.traefik_plugins.metadata.0.name}"
+additionalVolumeMounts:
+- name: plugins
+  mountPath: /plugins-storage
+securityContext:
+  seccompProfile:
+    type: RuntimeDefault
+providers:
+  kubernetesCRD:
+    enabled: true
+logs:
+  general:
+    level: "${var.traefik-log-level}"
+  access:
+    enabled: ${var.traefik-access-log}
+service:
+  spec:
+    externalTrafficPolicy: "${var.traefik-external-access-policy}"
+experimental:
+  plugins:
+    rewrite-body:
+      moduleName: "github.com/packruler/rewrite-body"
+      version: "v1.2.0"
+    crowdsec-bouncer-traefik-plugin:
+      moduleName: "github.com/maxlerebourg/crowdsec-bouncer-traefik-plugin"
+      version: "v1.3.5"
+EOF
 }
